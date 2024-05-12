b185744dc053baeff70b9f202917ec2d31cb7d11
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-05-13 00:44:32 +0300

    update_8

--- hw3_process.py
+++ hw3_process.py
@@ -1,4 +1,4 @@
-import multiprocessing
+from multiprocessing import Pool
 import time
    
 
@@ -20,6 +20,32 @@ def test_factorize():
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
+def factorize_parallel(*numbers):
+    with Pool() as pool:
+        result = pool.map(factorize_single, numbers)
+    return result
+
+
+def factorize_single(num):
+    factors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.append(i)
+    return factors
+
+if __name__ == '__main__':
+    test_factorize()
+    start_time = time.time()
+    result_sync = factorize(128, 255, 99999, 10651060)
+    end_time = time.time()
+    print("Synchronous execution time:", end_time - start_time)
+
+    start_time = time.time()
+    result_parallel = factorize_parallel(128, 255, 99999, 10651060)
+    end_time = time.time()
+    print("Parallel execution time:", end_time - start_time)
+
+    
 
 
             
